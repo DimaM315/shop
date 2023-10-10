420e4a1cf70b16d93298f8ef1e814b3bc851d4c6
Dmintiy <dima2000malischew2@mail.ru>
2023-10-10 21:02:07 +0500

    v1.0

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
 class  Configuration(object):
-	DEBUG = True
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:@localhost/BS_flask'
+	DEBUG = False
 	SECRET_KEY = 'secret'
 	
-	#flack sequrity
-	SECURITY_PASSWORD_SALT = 'sault'
-	SECURITY_PASSWORD_HASH = 'sha512_crypt'
